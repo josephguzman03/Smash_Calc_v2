bf9f09535f36fc497bb542171fc0b0401807d336
Joseph Guzman <j4guzman@ucsd.edu>
2025-04-15 20:21:41 -0700

    starter

---
+++ Smash_Calc_v2/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Smash Calculator v2</h1>
+<p>This is the v2 of the smash calcualtor. This will include:</p>
+<li>Flexible Players</li>
+<li>Fleible Buy-Ins </li>
+<li>Graphical representation </li>
+<li>Daily Logs</li>
+<br>
+<p> button </p>
+<script>
+    import { goto } from '$app/navigation';
+  </script>
+  
+  <button on:click={() => goto('/mid')}>There is Nothing after me</button>
+  
